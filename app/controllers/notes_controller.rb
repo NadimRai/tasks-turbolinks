6fb379a56073aea15ccb38da97100f8e00911356
NadimRai <rai_nadim@yahoo.com>
2017-07-18 20:19:21 -0700

    created Note index to display all Notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,15 @@
 class NotesController < ApplicationController
+	def index
+		@notes = Note.all 
+	end
+
+	def show
+		@note = Note.find(params[:id])
+	end
+
+	def destroy
+		@note = Note.find(params[:id])
+		@note.destroy
+		redirect_to notes_url 
+	end
 end
